80d086199701586dbdf40449b1ed4665a065ec58
DiegoBeltranC <dabcpop@gmail.com>
2024-07-17 23:31:22 -0500

    Terminada version1 usuarios index

--- Ciudadanos/usuario_index.php
+++ Ciudadanos/usuario_index.php
@@ -8,37 +8,47 @@
 </head>
 <body>
     <div class="nav">
-        <div>
-            <div class="user-icon-container"><img src="" alt=""></div>
+        <div class="user-container">
+            <div class="user-icon-container"><img src="./icons/profile.jpg" alt=""></div>
             <div class="user-name-container">
-                <p></p>
+                <p>Jonathan Cherriz Solis</p>
             </div>
         </div>
-        <div>
-            <button class=""></button>
+        <div class="log-out-container">
+            <button id="log-out">Cerrar Session</button>
         </div>
     </div>
 
     <div class="main-container">
         <div class="options-container">
             <div class="option-user">
-                <h2></h2>
-                <img src="" alt="">
+                <h2>Historial Reportes</h2>
+                <div class="option-img-container">
+                    <img src="../Icons/101671.png" alt="">
+                </div>
             </div>
             <div class="option-user">
-                <h2></h2>
-                <img src="" alt="">
+                <h2>Noticias</h2>
+                <div class="option-img-container">
+                    <img src="../Icons/Newspaper-icon.png" alt="">
+                </div>
             </div>
             <div class="option-user">
-                <h2></h2>
-                <img src="" alt="">
+                <h2>Usuario</h2>
+                <div class="option-img-container">
+                    <img src="../Icons/profile.jpg" alt="">
+                </div>
             </div>
         </div>
         <div class="map-container">
-            <img src="" alt="">
+            <div class="map-img-container">
+                <img src="../images/Screenshot 2024-07-17 230206.png" alt="">
+            </div>
         </div>
         <div class="new-report-btn-container">
-            <img src="" alt="" id="new-report-btn">
+            <div class="div-new-report">
+                <img src="../Icons/plus-icon-1024x1024-jdaf40nu.png" alt="" id="new-report-btn">
+            </div>
         </div>
     </div>
 </body>
