0a8001a937840eaa56fd19740a89af0cd551932c
DiegoBeltranC <dabcpop@gmail.com>
2024-07-17 22:13:54 -0500

    Ciudadanos anadidos

---
+++ Ciudadanos/usuario_index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <div class="nav">
+        <div>
+            <div class="user-icon-container"><img src="" alt=""></div>
+            <div class="user-name-container">
+                <p></p>
+            </div>
+        </div>
+        <div>
+            <button class=""></button>
+        </div>
+    </div>
+
+    <div class="main-container">
+        <div class="options-container">
+            <div class="option-user">
+                <h2></h2>
+                <img src="" alt="">
+            </div>
+            <div class="option-user">
+                <h2></h2>
+                <img src="" alt="">
+            </div>
+            <div class="option-user">
+                <h2></h2>
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="map-container">
+            <img src="" alt="">
+        </div>
+        <div class="new-report-btn-container">
+            <img src="" alt="" id="new-report-btn">
+        </div>
+    </div>
+</body>
+</html>
