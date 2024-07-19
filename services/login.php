ed8b45b58acf35b0bebe1b4ee937c2ef3a8c326d
arielrivero <aarm10@outlook.com>
2024-07-19 08:30:59 -0600

    nada importante

--- services/login.php
+++ services/login.php
@@ -1,19 +1,31 @@
 <?php
 session_start();
 
-$usuario_correcto = 'admin@gmail.com';
-$contraseña_correcta = '1234';
+$admin = 'admin@gmail.com';
+$contraseña_admin = '1234';
+
+$alumno = 'alumno@gmail.com';
+$contraseña_alumno = '1234';
+
+$trabajador = 'trabajador@gmail.com';
+$contraseña_trabajador = '1234';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $usuario = $_POST['gmail'];
     $contraseña = $_POST['password'];
 
-    if ($usuario === $usuario_correcto && $contraseña === $contraseña_correcta) {
+    if ($usuario === $admin && $contraseña === $contraseña_admin) {
         $_SESSION['usuario'] = $usuario;
         header('Location: ../views/estadisticas.php');
         exit;
-    } else {
-        echo "Usuario o contraseña incorrectos";
-    }
+    } else if($usuario === $alumno && $contraseña === $contraseña_alumno) {
+            $_SESSION['usuario'] = $usuario;
+            header('Location: ../views/informes.php');
+            exit;
+    } else if ($usuario === $trabajador && $contraseña === $contraseña_trabajador) {
+            $_SESSION['usuario'] = $usuario;
+            header('Location: ../views/usuarios.php');
+            exit;
+     }
 }
 ?>
